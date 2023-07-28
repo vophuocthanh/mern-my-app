fb2b205ef86fa07a49f27d7930a7638427e98647
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-07-28 10:23:47 +0700

    confirn eslint mui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,13 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react', 'react-hooks', 'react-refresh'],
   rules: {
+    //React
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
+    // Common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -34,6 +35,13 @@ module.exports = {
     'comma-dangle': 1,
     'comma-spacing': 1,
     'arrow-spacing': 1,
+    // Material
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@mui/*/*/*'],
+      },
+    ],
   },
 };
 1;
