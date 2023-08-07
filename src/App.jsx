e1c28fc92b06449741e0c8a267195fe366d96629
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-08-08 01:41:35 +0700

    refactor alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import ModeSelect from './components/ModeSelect';
-import Board from './pages/Boards/_id';
+import Board from '~/pages/Boards/_id';
 
 function App() {
   return (
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,7 +1,6 @@
 // Board details
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import AppBar from '../../components/AppBar';
+import AppBar from '~/components/AppBar';
 import BoardBar from './BoardBar';
 import BoardContent from './BoardContent';
 
