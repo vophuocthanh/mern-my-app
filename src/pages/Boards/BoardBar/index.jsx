1a11561f5a1b0fe3d4551443eee79e145cdb44cb
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2023-09-07 22:59:50 +0700

    code board content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -37,6 +37,9 @@ function BoardBar() {
         borderBottom: '1px solid #00bfa5',
         bgcolor: (theme) =>
           theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+        '&::-webkit-scrollbar-track': {
+          m: 2,
+        },
       }}
     >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
@@ -93,6 +96,11 @@ function BoardBar() {
               height: 34,
               fontSize: 16,
               border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': {
+                bgcolor: '#a4b0be',
+              },
             },
           }}
         >
